418813554a3cb77be091857e41326e452a80c1bf
Ankit-0216 <ankitsethi1602@gmail.com>
2024-04-16 00:41:05 +0530

    authorization based on orgId and tokenIdentifier

---
+++ convex/users.ts
@@ -0,0 +1,45 @@
+import { MutationCtx, QueryCtx, internalMutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+
+export const getUser = async (
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string
+) => {
+  const user = await ctx.db
+    .query("users")
+    .withIndex("by_tokenIdentifier", (q) =>
+      q.eq("tokenIdentifier", tokenIdentifier)
+    )
+    .first();
+
+  if (!user) {
+    throw new ConvexError("User is not defined");
+  }
+
+  return user;
+};
+
+// Create a new task with the given text
+export const createUser = internalMutation({
+  args: { tokenIdentifier: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      tokenIdentifier: args.tokenIdentifier,
+      orgIds: [],
+    });
+  },
+});
+
+export const addOrgIdToUser = internalMutation({
+  args: {
+    tokenIdentifier: v.string(),
+    orgId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await getUser(ctx, args.tokenIdentifier);
+
+    await ctx.db.patch(user._id, {
+      orgIds: [...user.orgIds, args.orgId],
+    });
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,23 @@ import {
   SignOutButton,
   SignedIn,
   SignedOut,
+  useOrganization,
+  useUser,
 } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -34,8 +44,10 @@ export default function Home() {
 
       <Button
         onClick={() => {
+          if (!orgId) return;
           createFile({
             name: "hello world",
+            orgId,
           });
         }}
       >
