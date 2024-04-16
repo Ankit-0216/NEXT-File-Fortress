ba0b81b4682c053a8d9ff30554b3b2bec9d35299
Ankit-0216 <ankitsethi1602@gmail.com>
2024-04-16 22:32:34 +0530

    Added upload file dialog

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,14 +1,34 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  OrganizationSwitcher,
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 function Header() {
   return (
     <div className="border-b py-4 bg-gray-200">
       <div className="items-center container mx-auto justify-between flex">
         <div>FileFortress</div>
+
         <div className="flex gap-2">
           <OrganizationSwitcher />
           <UserButton />
+          <SignedIn>
+            <SignOutButton>
+              <Button>Sign Out</Button>
+            </SignOutButton>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
         </div>
       </div>
     </div>
