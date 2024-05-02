8a79b5ac602b7876bdeb048dba966a2532bb5a86
Ankit-0216 <ankitsethi1602@gmail.com>
2024-05-02 23:42:53 +0530

    added landing page and footer

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -7,22 +7,25 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
+  useSession,
 } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
   return (
-    <div className="border-b py-3 bg-gray-200">
+    <div className="relative z-10 border-b py-3 bg-gray-200">
       <div className="items-center container mx-auto justify-between flex">
         <Link href="/" className="flex gap-2 items-center">
           <Image src="/logo.png" alt="File Fortress" width="45" height="5" />
           FileFortress
         </Link>
 
-        <Button variant={"ghost"}>
-          <Link href="/dashboard/files">Your files</Link>
-        </Button>
+        <SignedIn>
+          <Button variant={"ghost"}>
+            <Link href="/dashboard/files">Your files</Link>
+          </Button>
+        </SignedIn>
 
         <div className="flex gap-2">
           <OrganizationSwitcher />
